4a0720758a89f37a32b3daedb18959f5c85711ec
Manuel Ulate <fr3zs09@gmail.com>
2024-10-21 09:11:13 -0600

    feat:cron adapter pattern

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,18 @@
+import { CronService } from "./cron/cron-service";
 
 
 export class Server {
   
   public static start() {
     console.log('Server started');
+
+    CronService.createJob(
+      '*/3 * * * * *', 
+      () => {
+        console.log('Cron job running');
+      }
+    );
+
   }
 
 }
