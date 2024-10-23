8a505b8a41f0b5a3cffd91dc5be6fb1f7d8de31d
Manuel Ulate <fr3zs09@gmail.com>
2024-10-23 10:54:47 -0600

    feat(presentation):email service

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,7 +1,40 @@
-import {} from 'nodemailer';
+import nodemailer from 'nodemailer';
+
+interface SendEmailOptions {
+  to:       string;
+  subject:  string;
+  htmlBody: string;
+  // attachments?: Attachment[];
+}
 
 export class EmailService {
 
-  private transporter = node
+  private transporter = nodemailer.createTransport({
+    service: process.env.MAILER_SERVICE,
+    auth: {
+      user: process.env.MAILER_EMAIL,
+      pass: process.env.MAILER_SECRET_KEY,
+    }
+  });
+
+  async sendEmail( options:SendEmailOptions ):Promise<boolean> {
+
+    const { to, subject, htmlBody } = options;
+
+    try {
+      
+      const sentInfo = await this.transporter.sendMail({
+        to,
+        subject,
+        html: htmlBody,
+      });
+
+      return true;
+
+    } catch ( error ) {
+      
+      return false;
+    }
+  }
 
 }
