216355c57700932ea1fdf533a1cff183d5861f51
Manuel Ulate <fr3zs09@gmail.com>
2024-10-24 10:34:16 -0600

    feat(data): mongo log model

---
+++ src/data/mongo/models/log.model.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const logSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'low'
+  },
+  origin: String,
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+});
+
+export const LogModel = mongoose.model('Log', logSchema);
