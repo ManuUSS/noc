5616dde1b0cc4dcc6e60aa194ba612d279397742
Manuel Ulate <fr3zs09@gmail.com>
2024-10-26 10:38:29 -0600

    feat(presentation):mongo data source implemented

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,14 @@
+import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDataSource } from '../infraestructure/datasources/file-system.datasource';
+import { MongoLogDataSource } from '../infraestructure/datasources/mongo-log.datasource';
 import { LogRepositoryImplementation } from '../infraestructure/repositories/log.repository';
+import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
-const fileSystemLogRepository = new LogRepositoryImplementation(
-  new FileSystemDataSource()
+const logRepository = new LogRepositoryImplementation(
+  // new FileSystemDataSource()
+  new MongoLogDataSource()
 );
 const emailService = new EmailService();
 
@@ -13,23 +17,23 @@ export class Server {
   public static start() {
     console.log('Server started');
 
-    new SendEmailLogs(
-      emailService,
-      fileSystemLogRepository
-    ).execute(['llobozamora@gmail.com', 'imanuel.ulate@gmail.com'])
+    // new SendEmailLogs(
+    //   emailService,
+    //   fileSystemLogRepository
+    // ).execute(['llobozamora@gmail.com', 'imanuel.ulate@gmail.com'])
 
 
-    // CronService.createJob(
-    //   '*/3 * * * * *', 
-    //   () => {
-    //     const url = 'https://google.com';
-    //     new CheckService(
-    //       fileSystemLogRepository,
-    //       () => console.log(`${url} is available`), 
-    //       (error) => console.log(`Error: ${error}`)
-    //     ).execute( url );
-    //   }
-    // ).start();
+    CronService.createJob(
+      '*/3 * * * * *', 
+      () => {
+        const url = 'https://google.com';
+        new CheckService(
+          logRepository,
+          () => console.log(`${url} is available`), 
+          (error) => console.log(`Error: ${error}`)
+        ).execute( url );
+      }
+    ).start();
 
   }
 
