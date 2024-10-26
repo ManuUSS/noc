7a2f24d7f2b7ca457c0028e7b3ecab63308f942b
Manuel Ulate <fr3zs09@gmail.com>
2024-10-26 10:32:48 -0600

    feat(infrastructure):mogo log datasource

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { envs } from './config/plugins/envs.plugin';
 import { MongoDatabase } from './data/mongo';
+import { LogModel } from './data/mongo/models/log.model';
 import { Server } from './presentation/server';
 
 
@@ -13,6 +14,19 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME
   });
+
+
+  // const newLog = await LogModel.create({
+  //   message: 'Hello World',
+  //   origin: 'app.ts',
+  //   level: 'low'
+  // });
+
+  // await newLog.save();
+
+  // const logs = await LogModel.find();
+
+  
 
   Server.start();
 }
