a0d70d575682a6f303c4fc9af3941cae327bf5ae
Manuel Ulate <fr3zs09@gmail.com>
2024-10-30 09:29:36 -0600

    feat(infrastructure):file system data source test create files

--- src/infraestructure/datasources/file-system.datasource.test.ts
+++ src/infraestructure/datasources/file-system.datasource.test.ts
@@ -1,8 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { FileSystemDataSource } from './file-system.datasource';
 
 
 
 describe('file-system.datasource.ts', () => {
 
+  const logPath = path.join(__dirname, '../../../logs');
+
+  beforeEach(() => {
+    fs.rmSync( logPath, { recursive:true, force:true } );
+  });
+
+
+  test('should create a log files if they do not exists', () => {
+
+    new FileSystemDataSource();
+
+    const files = fs.readdirSync( logPath );
+
+    expect( files.length ).toBe( 3 );
+    expect( files ).toEqual(['logs-all.log', 'logs-medium.log', 'logs-high.log']);
+
+  });
 
 
 });
