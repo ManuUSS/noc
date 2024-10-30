09013d0e90492c00e01da435ae712b81c3a19718
Manuel Ulate <fr3zs09@gmail.com>
2024-10-30 09:21:23 -0600

    feat(infrastructure):mongo log data source test

--- src/infraestructure/datasources/mongo-log.datasource.test.ts
+++ src/infraestructure/datasources/mongo-log.datasource.test.ts
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
+
 import { envs } from '../../config/plugins/envs.plugin';
 import { MongoDatabase } from '../../data/mongo';
 import { MongoLogDataSource } from './mongo-log.datasource';
 import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+import { LogModel } from '../../data/mongo/models/log.model';
 
 
 describe('mongo-log.datasource', () => {
+  
+  const logDataSource = new MongoLogDataSource();
 
   beforeAll(async () => {
     await MongoDatabase.connect({
@@ -14,15 +18,16 @@ describe('mongo-log.datasource', () => {
     });
   });
 
+  afterEach(async () => {
+    await LogModel.deleteMany({});
+  });
+
   afterAll(async () => {
     await mongoose.connection.close();
   });
 
-
-
   test('should save a log', async () => {
 
-    const logDataSource = new MongoLogDataSource();
     const logSpy = jest.spyOn( console, 'log' );
 
     const log = new LogEntity({
@@ -37,6 +42,23 @@ describe('mongo-log.datasource', () => {
 
   });
 
+  test('should get logs by severity level', async () => {
+    
+    const log = new LogEntity({
+      message: 'test message',
+      level: LogSeverityLevel.low,
+      origin: 'test',
+    });
+
+    await logDataSource.saveLog( log );
+    await logDataSource.saveLog( log );
+
+    const logs = await logDataSource.getLogs( LogSeverityLevel.low );
+
+    expect( logs.length ).toBe( 2 );
+    expect( logs[0].level ).toBe( LogSeverityLevel.low );
+
+  });
 
 });
 
